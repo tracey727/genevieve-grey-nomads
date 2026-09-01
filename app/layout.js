e60138cc16b6e0f361cdb65c1572f298d595d32a
Tracey <positivity864@gmail.com>
2026-09-01 12:03:48 +1000

    Complete Budget Traveller install-name rename

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
   },
   appleWebApp: {
     capable: true,
-    title: 'Budget Travels',
+    title: 'Budget Traveller',
     statusBarStyle: 'black-translucent'
   }
 };
